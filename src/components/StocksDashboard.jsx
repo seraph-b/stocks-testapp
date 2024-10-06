77ab01c51e455b7c1ea39f68a9e175a30d7f2155
seraph-b <martin@bardon.cz>
2024-10-06 15:51:20 -0300

    added missing ?

--- src/components/StocksDashboard.jsx
+++ src/components/StocksDashboard.jsx
@@ -5,7 +5,7 @@ function StocksDashboard() {
   const [stocks, setStocks] = useState([]);
 
   const handleChange = async (event) => {
-    const stocksRes = await fetch('http://localhost:3000/' + new URLSearchParams({ query: event.target.value }));
+    const stocksRes = await fetch('http://localhost:3000/?' + new URLSearchParams({ query: event.target.value }));
     const stocksObj = await stocksRes.json();
     setStocks(stocksObj);
   };
