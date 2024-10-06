e5d94983c9b767abd507310aba1a576438ef79e4
seraph-b <martin@bardon.cz>
2024-10-06 21:18:43 +0200

    input sanitization and input population from storage

--- src/components/StocksDashboard.jsx
+++ src/components/StocksDashboard.jsx
@@ -2,15 +2,18 @@ import { useState, useEffect } from "react";
 import StocksList from "./StocksList";
 
 function StocksDashboard() {
+  const [searchTerm, setSearchTerm] = useState("");
   const [stocks, setStocks] = useState([]);
 
   useEffect(() => {
-    const storedStocks = localStorage.getItem("stocks")
-    storedStocks ? setStocks(JSON.parse(storedStocks)) : null;
+    const storedStocks = JSON.parse(localStorage.getItem("stocks"))
+    setStocks(storedStocks ? storedStocks : []);
+    setSearchTerm(storedStocks ? storedStocks.map((stock) => stock.symbol) : "");
   }, []);
 
   const handleStocksInputChange = async (event) => {
-    const stocksRes = await fetch('http://localhost:3000/?' + new URLSearchParams({ query: event.target.value }));
+    setSearchTerm(event.target.value);
+    const stocksRes = await fetch('http://localhost:3000/?' + new URLSearchParams({ query: event.target.value.replace(/\s+/g, "") }));
     const stocksObj = await stocksRes.json();
     setStocks(stocksObj);
     localStorage.setItem("stocks", JSON.stringify(stocksObj));
@@ -18,7 +21,7 @@ function StocksDashboard() {
 
   return (
     <div>
-      <input type="text" placeholder="SYMB" onChange={handleStocksInputChange} />
+      <input type="text" placeholder="SYMB" value={searchTerm} onChange={handleStocksInputChange} />
       <StocksList stocks={stocks} />
     </div>
   );
