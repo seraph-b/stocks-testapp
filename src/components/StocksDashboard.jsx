76ad3e1fc224c56ad58e77467a19102f3977d3d0
seraph-b <martin@bardon.cz>
2024-10-08 10:24:02 +0200

    limited updates to stocks object

--- src/components/StocksDashboard.jsx
+++ src/components/StocksDashboard.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import isEqual from "react-fast-compare";
 import StocksList from "./StocksList";
 
 function StocksDashboard() {
@@ -11,17 +12,20 @@ function StocksDashboard() {
     setSearchTerm(storedStocks ? storedStocks.map((stock) => stock.symbol) : "");
   }, []);
 
-  const handleStocksInputChange = async (event) => {
-    setSearchTerm(event.target.value);
-    const stocksRes = await fetch('http://localhost:3000/?' + new URLSearchParams({ query: event.target.value.replace(/\s+/g, "") }));
+  const handleStocksInputChange = async (searchValue) => {
+    setSearchTerm(searchValue);
+    const stocksRes = await fetch('http://localhost:3000/?' + new URLSearchParams({ query: searchValue.replace(/\s+/g, "") }));
     const stocksObj = await stocksRes.json();
-    setStocks(stocksObj);
-    localStorage.setItem("stocks", JSON.stringify(stocksObj));
+    if (!isEqual(stocksObj, stocks))
+    {
+      setStocks(stocksObj);
+      localStorage.setItem("stocks", JSON.stringify(stocksObj));
+    }
   };
 
   return (
     <div className="w-full mx-auto lg:w-2/3 2xl:w-1/3 flex flex-col space-y-2">
-      <input type="text" placeholder="SYMBL" value={searchTerm} onChange={handleStocksInputChange} className="w-full border-2 border-slate-700 bg-[#21252b] text-slate-200 rounded-md p-2" />
+      <input type="text" placeholder="SYMBL" value={searchTerm} onChange={(e) => {handleStocksInputChange(e.target.value)}} className="w-full border-2 border-slate-700 bg-[#21252b] text-slate-200 rounded-md p-2" />
       <div className="w-full">
         <StocksList stocks={stocks} />
       </div>
--- src/components/StocksList.jsx
+++ src/components/StocksList.jsx
@@ -1,3 +1,4 @@
+import { memo } from 'react';
 import StocksItem from './StocksItem';
 
 function StocksList({ stocks }) {
@@ -24,4 +25,4 @@ function StocksList({ stocks }) {
   );
 }
 
-export default StocksList;
+export default memo(StocksList);
